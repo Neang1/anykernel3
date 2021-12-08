5f8f4d646e157be802557e5bd5d68afb6763a3b9
Neang1 <82277200+Neang1@users.noreply.github.com>
2021-12-08 11:34:40 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -10,8 +10,9 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=lavender
-supported.versions=9 - 12
+device.name1=zerofltexx
+device.name2=zeroltexx
+supported.versions=11
 supported.patchlevels=
 '; } # end properties
 
